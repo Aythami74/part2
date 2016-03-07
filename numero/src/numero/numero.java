b80285df52b573ccf0ff6e2e572a629b783d1a30
Aythami74 <aythamisosa74@gmail.com>
2016-03-07 16:15:50 +0000

    Intento

---
+++ numero/src/numero/numero.java
@@ -0,0 +1,27 @@
+package numero;
+
+import java.util.Scanner;
+
+public class numero {
+	public static void main(String[] args) {
+		int i = 0;
+		int n = 0;
+		int numero = 0;
+		int suma = 0;
+		int multipli = 1;
+		System.out.println("¿Cuántos números quieres sumar y multiplicar?");
+		Scanner leer = new Scanner(System.in);
+		n = leer.nextInt();
+
+		System.out.println("Pon qué números son");
+
+		for (i = 1; i <= n; i = i + 1) {
+			suma = leer.nextInt();
+			numero += suma;
+			multipli = multipli * suma;
+		}
+		System.out.println("La suma es " + numero);
+		System.out.println("La multiplicacion es: " + multipli);
+		leer.close();
+	}
+}
